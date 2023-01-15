30881744d092c0c1cceaec8dda36ee3ba1b6e7e6
KiberJoy <kiberjoy@gmail.com>
2023-01-15 23:03:24 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test-app';
+
+  // stream$ = Observable.create((observer: Observer<number>) => {
+  //   observer.next(1);
+  //   setTimeout(() => {
+  //     observer.next(2);
+  //   }, 2000);
+  //   setTimeout(() => {
+  //     observer.next(3);
+  //   }, 3000);
+  //   setTimeout(() => {
+  //     observer.complete();
+  //   }, 4000);
+  // });
+
+  // subscription = this.stream$.subscribe((res: number) => console.log(res));
 }
