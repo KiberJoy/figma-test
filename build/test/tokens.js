3011d0583fd60e8237d1fbc5f331c3e2d79e59bf
KiberJoy <kiberjoy@gmail.com>
2023-01-16 22:25:22 +0300

    custom format

---
+++ build/test/tokens.js
@@ -0,0 +1,3 @@
+export const TestTokensBlack = rgba(21, 21, 32, 0.5);
+export const TestTokensRed = rgba(254, 0, 0, 0.5);
+export const TestTokensBlue = rgba(17, 77, 225, 0.5);
---
+++ sd.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  format: {
+    // Adding a custom format to show how to get an alias's name.
+    customFormat: function ({ dictionary, options }) {
+      const hex2rgba = (value, alpha = 0.5) => {
+        const [r, g, b] = value.match(/\w\w/g).map((x) => parseInt(x, 16));
+        return `rgba(${r},${g},${b},${alpha})`;
+      };
+      return dictionary.allTokens
+        .map((token) => {
+          let value = JSON.stringify(token.value);
+          // new option added to decide whether or not to output references
+
+          return `export const ${token.name} = ${hex2rgba(value, 0.5)};`;
+        })
+        .join(`\n`);
+    },
+  },
+
+  source: ["tokens/**/*.json"],
+  platforms: {
+    js: {
+      buildPath: "build/test/",
+      transformGroup: "js",
+      files: [
+        {
+          destination: "tokens.js",
+          format: "customFormat",
+        },
+      ],
+    },
+    scss: {
+      transformGroup: "scss",
+      buildPath: "build/test/",
+      files: [
+        {
+          destination: "tokens.scss",
+          format: "scss/variables",
+        },
+        {
+          destination: "tokens.scss",
+          format: "scss/variables",
+        },
+      ],
+    },
+  },
+};
